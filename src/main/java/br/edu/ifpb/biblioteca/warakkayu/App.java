875c0769e5da88e6ad455102eaba14215ea1426e
Gabriel-Aragao <batista.gabriel.aragao@gmail.com>
2025-06-24 11:34:58 -0300

    hotfix: Correção de imports não utilizados no App.java

--- src/main/java/br/edu/ifpb/biblioteca/warakkayu/App.java
+++ src/main/java/br/edu/ifpb/biblioteca/warakkayu/App.java
@@ -1,12 +1,4 @@
 package br.edu.ifpb.biblioteca.warakkayu;
-
-import br.edu.ifpb.biblioteca.warakkayu.dao.ObraDAO;
-import br.edu.ifpb.biblioteca.warakkayu.dao.Persistivel;
-import br.edu.ifpb.biblioteca.warakkayu.dao.UsuarioDAO;
-import br.edu.ifpb.biblioteca.warakkayu.model.Livro;
-import br.edu.ifpb.biblioteca.warakkayu.model.Obra;
-import br.edu.ifpb.biblioteca.warakkayu.model.Usuario;
-
 /**
  * Hello world!
  *
